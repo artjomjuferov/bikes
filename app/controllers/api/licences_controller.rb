5d78557b06b75e33999acce58e8632b782ed0803
Artjom Juferov <artjom.juferov@gmail.com>
2022-04-14 14:32:25 +0300

    Integrate CsvValidator into action

--- app/controllers/api/licences_controller.rb
+++ app/controllers/api/licences_controller.rb
@@ -2,5 +2,16 @@ class Api::LicencesController < ApplicationController
   include Pundit::Authorization
 
   def create
+    unless csv_validator.call
+      return head :unprocessable_entity
+    end
+
+    head :created
+  end
+
+  private
+
+  def csv_validator
+    CsvValidator.new(csv_string: params[:file].read)
   end
 end
